28b368f4f7d6605b26c9116823b5d57e069387df
clertonf <clerton.filho7@gmail.com>
2023-02-20 17:29:55 -0300

    Initial Commit

---
+++ ChallengeTime/ChallengeTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeTimeApp.swift
+//  ChallengeTime
+//
+//  Created by Clerton Filho on 20/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
